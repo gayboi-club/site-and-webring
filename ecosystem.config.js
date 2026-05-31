85c0e85c89113cf13e596b0e28d9c40209d148a7
Energyboy :3 <94549302+Energyboy-OwO@users.noreply.github.com>
2026-05-31 13:55:13 +0200

    bot update :3

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,33 @@
 module.exports = {
-  apps: [{
-    name: 'gayboi.club',
-    script: './server.js',
-    watch: false,
-    env: {
-      NODE_ENV: 'production',
-      PORT: 2999
+  apps: [
+    {
+      name: 'gayboi.club',
+      script: './server.js',
+      watch: false,
+      env: {
+        NODE_ENV: 'production',
+        PORT: 2999
+      },
+      error_file: './logs/error.log',
+      out_file: './logs/out.log',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss',
+      max_restarts: 10,
+      restart_delay: 2000
     },
-    error_file: './logs/error.log',
-    out_file: './logs/out.log',
-    log_date_format: 'YYYY-MM-DD HH:mm:ss',
-    max_restarts: 10,
-    restart_delay: 2000
-  }]
+    {
+      name: 'gayboi-bot',
+      script: './bot.js',
+      watch: false,
+      env: {
+        NODE_ENV: 'production',
+        DISCORD_BOT_TOKEN: process.env.DISCORD_BOT_TOKEN,
+        DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID
+      },
+      error_file: './logs/bot-error.log',
+      out_file: './logs/bot-out.log',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss',
+      max_restarts: 10,
+      restart_delay: 5000
+    }
+  ]
 };
